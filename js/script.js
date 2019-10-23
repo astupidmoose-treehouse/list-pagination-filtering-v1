64ed6cfee475f84857ba57e4d262ddbea2128e2f
Mike Olthoff <mike@olthoff.ca>
2019-10-23 11:53:48 -0700

    Add all "meeting" functionality

--- js/script.js
+++ js/script.js
@@ -1,49 +1,97 @@
-/******************************************
-Treehouse Techdegree:
-FSJS project 2 - List Filter and Pagination
-******************************************/
-   
-// Study guide for this project - https://drive.google.com/file/d/1OD1diUsTMdpfMDv677TfL1xO2CEkykSz/view?usp=sharing
+
+// We define the list, which is a selection of all list items in the page. 
+const list = document.querySelectorAll('li');
+
+// We define our maximum amount of items to list at a given time. 
+const maxItems = 10;
+
+// Define a default page, lets set it to 1. 
+const defaultPage = 2;
+
+// showPage function asks for the page number to be displayed. 
+const showPage = (page) =>{
+   // set the last item in the range based on page number
+   const lastItem = page * maxItems;
+   //set the first item based on page number
+   const firstItem = lastItem - maxItems;
+
+   // for each item in the list array, if the item is outside of the range we want to show, we add a display style of none, else, we leave the display style blank. 
+   for (let i = 0; i < list.length; i += 1){
+      if ([i] < firstItem || [i] >= lastItem){
+         list[i].style.display = "none";
+      } else {
+         list[i].style.display = "";
+      }
+   }
+}
+
+// Call the function with a default of the first page. Default value of 1 for first page.
+showPage(defaultPage); 
 
 
-/*** 
-   Add your global variables that store the DOM elements you will 
-   need to reference and/or manipulate. 
-   
-   But be mindful of which variables should be global and which 
-   should be locally scoped to one of the two main functions you're 
-   going to create. A good general rule of thumb is if the variable 
-   will only be used inside of a function, then it can be locally 
-   scoped to that function.
-***/
+// this function creates the needed elements for our buttons. 
 
+const appendPageLinks = (page) => {
 
+   // do the math to find out how many pages we will have
+   const buttonsNumber = Math.ceil(list.length / maxItems);
 
+   // create the div to store the buttons
+   const paginationDiv = document.createElement('div');
 
-/*** 
-   Create the `showPage` function to hide all of the items in the 
-   list except for the ten you want to show.
+   // append the div to the div with the "page" class
+   document.querySelector(".page").appendChild(paginationDiv);
 
-   Pro Tips: 
-     - Keep in mind that with a list of 54 students, the last page 
-       will only display four.
-     - Remember that the first student has an index of 0.
-     - Remember that a function `parameter` goes in the parens when 
-       you initially define the function, and it acts as a variable 
-       or a placeholder to represent the actual function `argument` 
-       that will be passed into the parens later when you call or 
-       "invoke" the function 
-***/
+   // give the div the "pagination class"
+   paginationDiv.className = "pagination";
 
+   // create the ul element
+   const ul = document.createElement('ul');
 
+   // append the ul to the div
+   paginationDiv.appendChild(ul);
 
+   // for each needed button, run the loop to create buttons
+   for (let i = 0; i < buttonsNumber; i+=1){
 
-/*** 
-   Create the `appendPageLinks function` to generate, append, and add 
-   functionality to the pagination buttons.
-***/
+      // define the page number, since the array starts at 0, add 1
+      const pageNumber = i + 1;
 
+      // create the li element
+      const li = document.createElement("li");
 
+      // append the li to the ul
+      ul.appendChild(li);
+
+      // create the a element
+      const a = document.createElement("a");
+
+      // append the to a to the li
+      li.appendChild(a);
+
+      // set the content of the button to match the page number
+      a.textContent = pageNumber;
+
+      // if the current page is the selected page, make sure the button is active
+      if (pageNumber === page){
+         a.className = "active";
+      }
+
+      // add a click listener
+      a.addEventListener("click", (e) => {
+         // find all "active" class items, and set them to blank
+         document.querySelector(".active").className = "";
+         // call the function again on the new page number
+         showPage(pageNumber);
+         // set the clicked button as the active button
+         event.target.className = "active";
+      })
+   }
+}
+
+// call the function to create the buttons. Default value of 1 for first page. 
+
+appendPageLinks(defaultPage);
 
 
 
